3959c49e553df5fe577c08e34c2746438ab57c97
Pavel Babushkin <darkdarin@tochka.com>
2019-09-23 14:30:09 +0500

    master: Fix не нужно парсить конфиг, если клиент не бущдет использован (правка, если запускать команды artisan без сконфигурированных клиентов, то команды падают)

--- src/JsonRpcClientServiceProvider.php
+++ src/JsonRpcClientServiceProvider.php
@@ -27,10 +27,10 @@ class JsonRpcClientServiceProvider extends ServiceProvider
         $clientName = config('jsonrpc-client.clientName', []);
 
         foreach ($services as $alias => $serviceConfig) {
-            $config = new ClientConfig($clientName, $alias, $serviceConfig);
-
-            if (class_exists($config->clientClass)) {
-                $this->app->singleton($config->clientClass, function () use ($config) {
+            $clientClass = $serviceConfig['clientClass'] ?? null;
+            if (class_exists($clientClass)) {
+                $this->app->singleton($clientClass, function () use ($clientName, $alias, $serviceConfig) {
+                    $config = new ClientConfig($clientName, $alias, $serviceConfig);
                     $client = new HttpClient();
                     $queryPreparer = $this->app->get($config->queryPreparer);
 
